7101eb49cea9f091808aafd40369ae3cbf61a66b
Hasyim Sri Wahyudi <hasyimsriewahyudi@gmail.com>
2022-07-05 16:39:58 +0700

    update file

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -6,8 +6,49 @@ const Price = () => {
         <div className='max-w-[1168px] px-4 mx-auto py-28'>
             <h1 className='fontPrimary text-4xl text-center textPrimary md:w-6/12 mx-auto'>Main catur sambil bersepeda, beda harga beda fitur</h1>
             <p className='text-lg text-center text-slate-500 py-4 md:w-8/12 mx-auto'>Aplikasi ini gratis sebenernya, brodie. Tapi kalo mau bayar gapapa juga sih, untuk gantinya kami kasih fitur yang spesial buat kamu!</p>
-            <div className='grid md:grid-cols-3 gap-6'>
-                <div>✅</div>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6 py-4'>
+                <div className='p-10 bg-white rounded-md shadow-md'>
+                  <h3 className='text-sm textSecondary font-semibold'>RINDURATU</h3>
+                  <h1 className='fontPrimary textPrimary text-4xl py-4'>Free</h1>
+                  <p className='text-lg fontSecondary text-slate-500'>Akses ke semua fitur gratis tapi data lo dijual ke agen khusus US.</p>
+                  <div className='py-4'>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Jadwal Tak Terbatas</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Fitur Notifikasi (Kadang Telat)</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Mode Gelap</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Bisa Topup Pulsa</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Bisa Beli Token Listrik</p>
+                    <p className='text-lg text-slate-700 fontSecondary pt-1 pb-8'>✅ Data Lo Dijual</p>
+                    <button className='buttonPrimary w-full'>Download</button>
+                  </div>
+                </div>
+                <div className='p-10 bg-white rounded-md shadow-md'>
+                  <h3 className='text-sm textSecondary font-semibold'>RINDURATU</h3>
+                  <h1 className='fontPrimary textPrimary text-4xl py-4'>Free</h1>
+                  <p className='text-lg fontSecondary text-slate-500'>Akses ke semua fitur gratis tapi data lo dijual ke agen khusus US.</p>
+                  <div className='py-4'>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Jadwal Tak Terbatas</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Fitur Notifikasi (Kadang Telat)</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Mode Gelap</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Bisa Topup Pulsa</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Bisa Beli Token Listrik</p>
+                    <p className='text-lg text-slate-700 fontSecondary pt-1 pb-8'>✅ Data Lo Dijual</p>
+                    <button className='buttonPrimary w-full'>Download</button>
+                  </div>
+                </div>
+                <div className='p-10 bg-white rounded-md shadow-md'>
+                  <h3 className='text-sm textSecondary font-semibold'>RINDURATU</h3>
+                  <h1 className='fontPrimary textPrimary text-4xl py-4'>Free</h1>
+                  <p className='text-lg fontSecondary text-slate-500'>Akses ke semua fitur gratis tapi data lo dijual ke agen khusus US.</p>
+                  <div className='py-4'>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Jadwal Tak Terbatas</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Fitur Notifikasi (Kadang Telat)</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Mode Gelap</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Bisa Topup Pulsa</p>
+                    <p className='text-lg text-slate-700 fontSecondary py-1'>✅ Bisa Beli Token Listrik</p>
+                    <p className='text-lg text-slate-700 fontSecondary pt-1 pb-8'>✅ Data Lo Dijual</p>
+                    <button className='buttonPrimary w-full'>Download</button>
+                  </div>
+                </div>
             </div>
         </div>
     </div>
